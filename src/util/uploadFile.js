b35d26955bb9da2a3fb6c16e5dac741a3ebd67ef
dzucconi <mail@damonzucconi.com>
2019-05-03 21:31:47 -0400

    Converts to async

--- src/util/uploadFile.js
+++ src/util/uploadFile.js
@@ -1,26 +1,26 @@
 import axios from 'axios'
 
-export const uploadFile = ({
+export const uploadFile = async ({
   file,
   presignedUploadUrl,
   onFileProgress,
   onFileDone,
 }) => {
-  return axios
-    .put(presignedUploadUrl, file, {
-      headers: {
-        'Content-Type': file.type,
-      },
-      onUploadProgress: progressEvent => {
-        const progress = Math.round(
-          (progressEvent.loaded * 100) / progressEvent.total
-        )
-        onFileProgress(progress)
-      },
-    })
-    .then(() => {
-      const url = presignedUploadUrl.split('?')[0]
-      onFileDone(url)
-      return { file, url }
-    })
+  await axios.put(presignedUploadUrl, file, {
+    headers: {
+      'Content-Type': file.type,
+    },
+    onUploadProgress: progressEvent_1 => {
+      const progress = Math.round(
+        (progressEvent_1.loaded * 100) / progressEvent_1.total
+      )
+      onFileProgress(progress)
+    },
+  })
+
+  const url = presignedUploadUrl.split('?')[0]
+
+  onFileDone(url)
+
+  return { file, url }
 }
