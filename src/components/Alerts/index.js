84ab8a54cfc48f90aef1d0c720244037a73824b4
dzucconi <mail@damonzucconi.com>
2019-05-10 08:36:58 -0400

    Allows for alerts column to be independently scrolled in the event it overflows

--- src/components/Alerts/index.js
+++ src/components/Alerts/index.js
@@ -15,8 +15,11 @@ const Container = styled(Box).attrs({
   position: fixed;
   top: 0;
   right: 0;
+  bottom: 0;
   width: 20em;
   z-index: 1;
+  overflow-y: auto;
+  -webkit-overflow-scrolling: touch;
 `
 
 export const WithAlerts = WrappedComponent => {
