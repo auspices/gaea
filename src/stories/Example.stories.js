8d03fdd9706701550e54f165e0f1bb42a13748af
dzucconi <mail@damonzucconi.com>
2019-05-10 08:36:38 -0400

    Adds example story

---
+++ src/stories/Example.stories.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import Box from 'components/UI/Box'
+
+storiesOf('Example', module).add('default', () => (
+  <>
+    <Box>Example</Box>
+  </>
+))
