9c23d15877bce1147d5f33599078ec017bee84cf
dzucconi <mail@damonzucconi.com>
2019-07-28 16:10:00 -0400

    Styles text container

--- src/components/ContentEntity/components/ContentEntityText/index.js
+++ src/components/ContentEntity/components/ContentEntityText/index.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import { Box } from 'components/UI/Box'
 
+const Container = styled(Box).attrs({
+  p: 6,
+  lineHeight: 2,
+})`
+  flex: 1;
+  width: 80ch;
+  max-width: 100%;
+  border: 1px solid black;
+`
+
 export const ContentEntityText = ({ text, ...rest }) => {
-  return <Box {...rest}>{text.body}</Box>
+  return <Container {...rest}>{text.body}</Container>
 }
