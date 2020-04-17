8c73f485892b331a594a2bd122cbaf5dfaa3331d
dzucconi <mail@damonzucconi.com>
2020-04-17 15:02:56 -0400

    Dynamically sets placeholder height

--- src/components/BottomNav/BottomNav.tsx
+++ src/components/BottomNav/BottomNav.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styled from 'styled-components'
 import { Box, BoxProps, Pill } from '@auspices/eos'
 import { themeGet } from '@styled-system/theme-get'
@@ -20,23 +20,43 @@ const Container = styled(Box)`
 
 export type BottomNavProps = BoxProps
 
-export const BottomNav: React.FC<BottomNavProps> = ({ children, ...rest }) => (
-  <>
-    <Pill opacity={0} color="tertiary" borderColor="tertiary" zIndex={-1}>
-      the end
-    </Pill>
+export const BottomNav: React.FC<BottomNavProps> = ({ children, ...rest }) => {
+  const ref = useRef<HTMLDivElement | null>(null)
 
-    <Container
-      position="fixed"
-      bottom={0}
-      left={0}
-      right={0}
-      px={[0, 0, 2, 4]}
-      pb={[0, 0, 2, 4]}
-      zIndex={Z.BOTTOM_NAV}
-      {...rest}
-    >
-      {children}
-    </Container>
-  </>
-)
+  const [placeholderHeight, setPlaceholderHeight] = useState(0)
+
+  useEffect(() => {
+    if (!ref.current) return
+
+    const { height, paddingBottom } = getComputedStyle(ref.current)
+    setPlaceholderHeight(parseFloat(height) - parseFloat(paddingBottom))
+  }, [])
+
+  return (
+    <>
+      <Pill
+        opacity={0}
+        color="tertiary"
+        borderColor="tertiary"
+        zIndex={-1}
+        height={placeholderHeight}
+      >
+        the end
+      </Pill>
+
+      <Container
+        ref={ref}
+        position="fixed"
+        bottom={0}
+        left={0}
+        right={0}
+        px={[0, 0, 2, 4]}
+        pb={[0, 0, 2, 4]}
+        zIndex={Z.BOTTOM_NAV}
+        {...rest}
+      >
+        {children}
+      </Container>
+    </>
+  )
+}
