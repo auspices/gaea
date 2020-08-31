e7c47ef87906292f6bab9b13e779bbea97a4a419
dzucconi <mail@damonzucconi.com>
2020-08-30 20:40:33 -0400

    Simplifies usage

--- src/hooks/useHrefs.ts
+++ src/hooks/useHrefs.ts
@@ -1,14 +1,25 @@
-const GLYPH_ENDPOINT = 'https://glyph.labs.auspic.es'
+export const GLYPH_ENDPOINT = 'https://glyph.labs.auspic.es'
+
+export const root = () => '/'
+export const login = () => '/login'
+export const account = () => '/account'
+export const collections = () => `/xs`
+export const collection = (id: string) => `/xs/${id}`
+export const collectionSettings = (id: string) => `/xs/${id}/settings`
+export const content = (id: number) => `/x/${id}`
+export const data = (key: string) => `${GLYPH_ENDPOINT}/graph/${key}`
+
+export const HREFS = {
+  root,
+  login,
+  account,
+  collections,
+  collection,
+  collectionSettings,
+  content,
+  data,
+}
 
 export const useHrefs = () => {
-  return {
-    root: () => '/',
-    login: () => '/login',
-    account: () => '/account',
-    collections: () => `/xs`,
-    collection: (id: string) => `/xs/${id}`,
-    collectionSettings: (id: string) => `/xs/${id}/settings`,
-    content: (id: number) => `/x/${id}`,
-    data: (key: string) => `${GLYPH_ENDPOINT}/graph/${key}`,
-  }
+  return HREFS
 }
