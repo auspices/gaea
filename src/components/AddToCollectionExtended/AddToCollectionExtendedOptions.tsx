2e9cc1a2faecb467587a4f65fe0a8267fdbcb3d8
dzucconi <mail@damonzucconi.com>
2020-09-06 14:58:28 -0400

    Extracts mutations into hook

--- src/components/AddToCollectionExtended/AddToCollectionExtendedOptions.tsx
+++ src/components/AddToCollectionExtended/AddToCollectionExtendedOptions.tsx
@@ -1,39 +1,7 @@
 import React, { useCallback, useMemo } from 'react'
-import gql from 'graphql-tag'
-import { useMutation } from '@apollo/client'
-import { Button, Stack, StackProps, useAlerts } from '@auspices/eos'
+import { Button, Stack, StackProps } from '@auspices/eos'
 import { useKeyboardListNavigation } from 'use-keyboard-list-navigation'
-import { useContextualRef, useRefetch } from '../../hooks'
-import { errorMessage } from '../../util/errors'
-import { AddCollectionToCollectionMutation } from '../../generated/types/AddCollectionToCollectionMutation'
-import { CreateCollectionToAddMutation } from '../../generated/types/CreateCollectionToAddMutation'
-import { useParams } from 'react-router'
-
-export const CREATE_COLLECTION_TO_ADD_MUTATION = gql`
-  mutation CreateCollectionToAddMutation($title: String!) {
-    createCollection(input: { title: $title }) {
-      collection {
-        id
-        name
-      }
-    }
-  }
-`
-
-export const ADD_COLLECTION_TO_COLLECTION_MUTATION = gql`
-  mutation AddCollectionToCollectionMutation($parentId: ID!, $childId: ID!) {
-    addEntityToCollection(
-      input: { id: $parentId, entity: { id: $childId, type: COLLECTION } }
-    ) {
-      collection {
-        id
-      }
-      content {
-        id
-      }
-    }
-  }
-`
+import { useContextualRef, useCreateAndAddCollection } from '../../hooks'
 
 export type AddToCollectionExtendedOption = {
   label: string
@@ -52,91 +20,32 @@ export const AddToCollectionExtendedOptions: React.FC<AddToCollectionExtendedOpt
   onDone,
   ...rest
 }) => {
-  const { id = '' } = useParams<{ id: string }>()
-  const { refetch } = useRefetch()
-  const { sendError, sendNotification } = useAlerts()
+  const {
+    addCollectionToCollection,
+    createAndAddCollectionToCollection,
+  } = useCreateAndAddCollection()
 
   const { getContextualRef } = useContextualRef()
   const inputRef = getContextualRef('collectionInput')
-
-  const [createCollectionToAddMutation] = useMutation<
-    CreateCollectionToAddMutation
-  >(CREATE_COLLECTION_TO_ADD_MUTATION)
-
-  const [addCollectionToCollectionMutation] = useMutation<
-    AddCollectionToCollectionMutation
-  >(ADD_COLLECTION_TO_COLLECTION_MUTATION)
-
-  const handleCreateCollectionToAdd = useCallback(
-    async (title: string) => {
-      const { data } = await createCollectionToAddMutation({
-        variables: { title },
-      })
-      if (!data || !data.createCollection)
-        throw new Error(`unable to create ${title}`)
-      return data.createCollection.collection
-    },
-    [createCollectionToAddMutation]
-  )
-
-  const handleAddCollectionToCollection = useCallback(
-    async (childId: number, childName: string) => {
-      try {
-        await addCollectionToCollectionMutation({
-          variables: { parentId: id, childId },
-        })
-        sendNotification({ body: `successfully added ${childName}` })
-      } catch (err) {
-        sendError({ body: errorMessage(err) })
-      }
-
-      refetch()
-      onDone()
-    },
-    [
-      addCollectionToCollectionMutation,
-      id,
-      onDone,
-      refetch,
-      sendError,
-      sendNotification,
-    ]
-  )
-
-  const handleCreateAndAddCollectionToCollection = useCallback(
-    async (title: string) => {
-      try {
-        sendNotification({ body: `creating ${title}` })
-        const { id, name } = await handleCreateCollectionToAdd(title)
-        sendNotification({ body: `successfully created ${title}` })
-        handleAddCollectionToCollection(id, name)
-      } catch (err) {
-        sendError({ body: errorMessage(err) })
-      }
-    },
-    [
-      handleAddCollectionToCollection,
-      handleCreateCollectionToAdd,
-      sendError,
-      sendNotification,
-    ]
-  )
 
   const list: AddToCollectionExtendedOption[] = useMemo(
     () => [
       ...collections.map(({ id: childId, name: childName }) => ({
         label: `add ${childName}`,
-        onClick: () => handleAddCollectionToCollection(childId, childName),
+        onClick: () =>
+          addCollectionToCollection(childId, childName).then(() => onDone()),
       })),
       {
         label: `create and add collection “${value}”`,
-        onClick: () => handleCreateAndAddCollectionToCollection(value),
+        onClick: () =>
+          createAndAddCollectionToCollection(value).then(() => onDone()),
       },
     ],
     [
+      addCollectionToCollection,
       collections,
-      handleAddCollectionToCollection,
-      handleCreateAndAddCollectionToCollection,
+      createAndAddCollectionToCollection,
+      onDone,
       value,
     ]
   )
---
+++ src/hooks/useCreateAndAddCollection.ts
@@ -0,0 +1,113 @@
+import { useCallback } from 'react'
+import gql from 'graphql-tag'
+import { useMutation } from '@apollo/client'
+import { useAlerts } from '@auspices/eos'
+import { useRefetch } from './useRefetch'
+import { errorMessage } from '../util/errors'
+import { AddCollectionToCollectionMutation } from '../generated/types/AddCollectionToCollectionMutation'
+import { CreateCollectionToAddMutation } from '../generated/types/CreateCollectionToAddMutation'
+import { useParams } from 'react-router'
+import { useMatchesPath } from './useMatchesPath'
+import { PATTERNS } from './useHrefs'
+
+export const CREATE_COLLECTION_TO_ADD_MUTATION = gql`
+  mutation CreateCollectionToAddMutation($title: String!) {
+    createCollection(input: { title: $title }) {
+      collection {
+        id
+        name
+      }
+    }
+  }
+`
+
+export const ADD_COLLECTION_TO_COLLECTION_MUTATION = gql`
+  mutation AddCollectionToCollectionMutation($parentId: ID!, $childId: ID!) {
+    addEntityToCollection(
+      input: { id: $parentId, entity: { id: $childId, type: COLLECTION } }
+    ) {
+      collection {
+        id
+      }
+      content {
+        id
+      }
+    }
+  }
+`
+
+export const useCreateAndAddCollection = () => {
+  const { matches } = useMatchesPath()
+  const match = matches.collection
+  const { id = '' } = match ? match.params : {}
+
+  const { refetch } = useRefetch()
+  const { sendError, sendNotification } = useAlerts()
+
+  const [createCollectionToAddMutation] = useMutation<
+    CreateCollectionToAddMutation
+  >(CREATE_COLLECTION_TO_ADD_MUTATION)
+
+  const [addCollectionToCollectionMutation] = useMutation<
+    AddCollectionToCollectionMutation
+  >(ADD_COLLECTION_TO_COLLECTION_MUTATION)
+
+  const handleCreateCollectionToAdd = useCallback(
+    async (title: string) => {
+      const { data } = await createCollectionToAddMutation({
+        variables: { title },
+      })
+      if (!data || !data.createCollection)
+        throw new Error(`unable to create ${title}`)
+      return data.createCollection.collection
+    },
+    [createCollectionToAddMutation]
+  )
+
+  const handleAddCollectionToCollection = useCallback(
+    async (childId: number, childName: string) => {
+      try {
+        await addCollectionToCollectionMutation({
+          variables: { parentId: id, childId },
+        })
+        sendNotification({ body: `successfully added ${childName}` })
+      } catch (err) {
+        sendError({ body: errorMessage(err) })
+      }
+
+      return refetch()
+    },
+    [
+      addCollectionToCollectionMutation,
+      id,
+      refetch,
+      sendError,
+      sendNotification,
+    ]
+  )
+
+  const handleCreateAndAddCollectionToCollection = useCallback(
+    async (title: string) => {
+      try {
+        sendNotification({ body: `creating ${title}` })
+        const { id, name } = await handleCreateCollectionToAdd(title)
+        sendNotification({ body: `successfully created ${title}` })
+        handleAddCollectionToCollection(id, name)
+      } catch (err) {
+        sendError({ body: errorMessage(err) })
+      }
+    },
+    [
+      handleAddCollectionToCollection,
+      handleCreateCollectionToAdd,
+      sendError,
+      sendNotification,
+    ]
+  )
+
+  return {
+    createCollection: handleCreateCollectionToAdd,
+    addCollectionToCollection: handleAddCollectionToCollection,
+    createAndAddCollectionToCollection: handleCreateAndAddCollectionToCollection,
+  }
+}
