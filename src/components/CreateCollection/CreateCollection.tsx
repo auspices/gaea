0eadcdc607cf77d03075738f22a3dfc9b260ba4b
dzucconi <mail@damonzucconi.com>
2021-01-04 21:34:01 -0500

    Adds confirmation step to homepage create

--- src/components/CreateCollection/CreateCollection.tsx
+++ src/components/CreateCollection/CreateCollection.tsx
@@ -2,7 +2,7 @@ import React, { useCallback, useState } from 'react'
 import gql from 'graphql-tag'
 import { useMutation } from '@apollo/client'
 import { useHistory } from 'react-router-dom'
-import { Button, ClearableInput, useAlerts } from '@auspices/eos'
+import { Button, ClearableInput, useAlerts, useConfirm } from '@auspices/eos'
 import { useContextualRef, useHrefs } from '../../hooks'
 import { errorMessage } from '../../util/errors'
 import { Form } from '../Form'
@@ -11,6 +11,7 @@ import {
   CreateCollectionMutation,
   CreateCollectionMutationVariables,
 } from '../../generated/types/CreateCollectionMutation'
+import { Z } from '../../util/zIndexes'
 
 export const CREATE_COLLECTION_MUTATION = gql`
   mutation CreateCollectionMutation($title: String!) {
@@ -62,52 +63,66 @@ export const CreateCollection: React.FC<CreateCollectionProps> = ({
     [onChange]
   )
 
-  const handleSubmit = useCallback(
-    async (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault()
-      setMode(Mode.Creating)
+  const handleConfirm = useCallback(async () => {
+    setMode(Mode.Creating)
 
-      try {
-        const { data } = await createCollection({ variables: { title: value } })
-        const { collection } = data!.createCollection!
-        sendNotification({ body: `successfully created ${collection.title}` })
-        history.push(hrefs.collection(collection.slug))
-      } catch (err) {
-        setMode(Mode.Error)
-        sendError({ body: errorMessage(err) })
-      }
-    },
-    [createCollection, history, hrefs, sendError, sendNotification, value]
-  )
+    try {
+      const { data } = await createCollection({ variables: { title: value } })
+      const { collection } = data!.createCollection!
+      sendNotification({ body: `successfully created ${collection.title}` })
+      history.push(hrefs.collection(collection.slug))
+    } catch (err) {
+      setMode(Mode.Error)
+      sendError({ body: errorMessage(err) })
+    }
+  }, [createCollection, history, hrefs, sendError, sendNotification, value])
+
+  const { requestConfirmation, Confirmation } = useConfirm({
+    onConfirm: handleConfirm,
+  })
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    requestConfirmation()
+  }
 
   return (
-    <Form onSubmit={handleSubmit} {...rest}>
-      <ClearableInput
-        ref={setContextualRef('primaryInput')}
-        name="title"
-        placeholder="find or create collection"
-        onChange={handleChange}
-        disabled={mode === Mode.Creating}
-        required
-        autoFocus
-        autoComplete="off"
-        title=""
-        borderWidth={0}
-        width={0}
-        flex={1}
-      />
-
-      {value !== '' && (
-        <Button
-          borderWidth={0}
-          borderLeft="1px solid"
-          type="submit"
+    <>
+      <Form onSubmit={handleSubmit} {...rest}>
+        <ClearableInput
+          ref={setContextualRef('primaryInput')}
+          name="title"
+          placeholder="find or create collection"
+          onChange={handleChange}
           disabled={mode === Mode.Creating}
-        >
-          {(mode === Mode.Resting || mode === Mode.Creating) && 'add'}
-          {mode === Mode.Error && 'error'}
-        </Button>
-      )}
-    </Form>
+          required
+          autoFocus
+          autoComplete="off"
+          title=""
+          borderWidth={0}
+          width={0}
+          flex={1}
+        />
+
+        {value !== '' && (
+          <Button
+            borderWidth={0}
+            borderLeft="1px solid"
+            type="submit"
+            disabled={mode === Mode.Creating}
+          >
+            {
+              {
+                [Mode.Resting]: 'add',
+                [Mode.Creating]: 'adding',
+                [Mode.Error]: 'error',
+              }[mode]
+            }
+          </Button>
+        )}
+      </Form>
+
+      <Confirmation zIndex={Z.MODAL} />
+    </>
   )
 }
